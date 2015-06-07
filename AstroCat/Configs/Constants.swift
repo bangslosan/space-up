3004dfbce1443ec1a0d1ac29e765ef673b86e972
davidchin <davidchin.yf@gmail.com>
2015-06-07 14:36:34 +1000

    Sound settings

--- AstroCat/Configs/Constants.swift
+++ AstroCat/Configs/Constants.swift
@@ -12,7 +12,7 @@ import UIKit
 let MainBundleIdentifier = NSBundle.mainBundle().bundleIdentifier!
 
 // MARK: - Size
-let SceneSize = CGSize(width: 768, height: 1024)
+let SceneSize = CGSize(width: 720, height: 1280) // CGSize(width: 768, height: 1024)
 let WorldArea = CGRect(x: 0, y: 0, width: SceneSize.width * 2, height: SceneSize.height)
 
 // MARK: - Archive
--- AstroCat/Scenes/StartScene.swift
+++ AstroCat/Scenes/StartScene.swift
@@ -15,6 +15,8 @@ class StartScene: SKScene, ButtonDelegate {
   // MARK: - Immutable var
   let startButton = TextButtonNode(size: CGSize(width: 300, height: 60))
   let leaderboardButton = TextButtonNode(size: CGSize(width: 300, height: 60))
+  let soundButton = TextButtonNode(size: CGSize(width: 300, height: 60))
+  let musicButton = TextButtonNode(size: CGSize(width: 300, height: 60))
   
   // MARK: - View
   override func didMoveToView(view: SKView) {
@@ -22,15 +24,27 @@ class StartScene: SKScene, ButtonDelegate {
     
     // Start button
     startButton.label.text = "Start Game"
-    startButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY)
+    startButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY + 100)
     startButton.delegate = self
     addChild(startButton)
     
     // Leaderboard button
     leaderboardButton.label.text = "Leaderboard"
-    leaderboardButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY - 100)
+    leaderboardButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY)
     leaderboardButton.delegate = self
     addChild(leaderboardButton)
+    
+    // Sound button
+    soundButton.label.text = isSoundEnabled() ? "Sound On" : "Sound Off"
+    soundButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY - 100)
+    soundButton.delegate = self
+    addChild(soundButton)
+    
+    // Music button
+    musicButton.label.text = isMusicEnabled() ? "Music On" : "Music Off"
+    musicButton.position = CGPoint(x: screenFrame.midX, y: screenFrame.midY - 200)
+    musicButton.delegate = self
+    addChild(musicButton)
   }
   
   // MARK: - ButtonDelegate
@@ -44,8 +58,43 @@ class StartScene: SKScene, ButtonDelegate {
     case leaderboardButton:
       startSceneDelegate?.startSceneDidRequestLeaderboard?(self)
       
+    case soundButton:
+      toggleSound()
+      
+    case musicButton:
+      toggleMusic()
+      
     default:
       break
     }
+  }
+  
+  // MARK: - Sound
+  func toggleSound() {
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+
+    if isSoundEnabled() {
+      userDefaults.setValue(true, forKey: KeyForUserDefaults.SoundDisabled)
+      soundButton.label.text = "Music Off"
+    } else {
+      userDefaults.setValue(false, forKey: KeyForUserDefaults.SoundDisabled)
+      soundButton.label.text = "Music On"
+    }
+    
+    userDefaults.synchronize()
+  }
+  
+  func toggleMusic() {
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+
+    if isMusicEnabled() {
+      userDefaults.setValue(true, forKey: KeyForUserDefaults.MusicDisabled)
+      musicButton.label.text = "Music Off"
+    } else {
+      userDefaults.setValue(false, forKey: KeyForUserDefaults.MusicDisabled)
+      musicButton.label.text = "Music On"
+    }
+    
+    userDefaults.synchronize()
   }
 }
